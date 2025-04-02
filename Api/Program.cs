be6f02dee276f49c7103e3426e8e7c63535cad1d
Alexey <levalex814@gmail.com>
2025-04-02 15:59:53 +0300

    swagger settings

--- Api/Program.cs
+++ Api/Program.cs
@@ -85,7 +85,7 @@ app.MapControllers();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/calendar-service/swagger/v1/swagger.json", "My API V1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
     c.RoutePrefix = "swagger";
 });
 app.UseMiddleware<ExceptionHandlingMiddleware>();
