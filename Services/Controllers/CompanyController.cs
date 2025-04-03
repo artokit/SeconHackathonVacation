15e5a5d189981d080581426b2d285172b45257b5
Dmitry1850 <afsfhdjkvgk@gmail.com>
2025-04-03 07:30:42 +0300

    New gitignore

---
+++ Services/Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Services.Exceptions.Abstractions;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class CompanyController : ControllerBase
+{
+
+
+}
