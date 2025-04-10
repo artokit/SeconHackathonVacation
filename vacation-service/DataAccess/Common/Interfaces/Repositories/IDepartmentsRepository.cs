723caf3dc3b53815684d48281eb545c721ed9340
Alexey <levalex814@gmail.com>
2025-04-10 22:48:42 +0300

    GetAllDepartmentByCompanyId

--- vacation-service/DataAccess/Common/Interfaces/Repositories/IDepartmentsRepository.cs
+++ vacation-service/DataAccess/Common/Interfaces/Repositories/IDepartmentsRepository.cs
@@ -5,7 +5,8 @@ namespace DataAccess.Common.Interfaces.Repositories;
 public interface IDepartmentsRepository
 {
     public Task<DbDepartment?> GetDepartmentByIdAsync(Guid id);
-    public Task<List<DbDepartment>> GetAllDepartments();
+    public Task<List<DbDepartment>> GetAllDepartments(Guid id);
+    public Task<Guid> GetCompanyIdByUserId(Guid id);
     public Task<DbDepartment> CreateDepartmentAsync(DbDepartment department);
     public Task<DbDepartment> UpdateDepartmentAsync(Guid id, DbDepartment department);
     public Task DeleteDepartmentAsync(Guid id);
